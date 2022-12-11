c84ea0a51dcf59da0a934d64593fb3d3a34cbd94
Pablo <pablo.munoz@davinci.edu.ar>
2022-12-10 22:28:26 -0300

    cambios

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using FINAL_MVC.Models;
+﻿using FINAL_MVC.Data;
+using FINAL_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,12 +7,17 @@ namespace FINAL_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        // private readonly ILogger<HomeController> _logger;
+        private readonly Context _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(Context context)
         {
-            _logger = logger;
+            _context = context;
         }
+       // public HomeController(ILogger<HomeController> logger)
+       //{
+       //   _logger = logger;
+       // }
 
         public IActionResult Index()
         {
@@ -28,5 +34,44 @@ namespace FINAL_MVC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                using (DB_Entities db = new DB_Entities())
+                {
+                    var obj = db.UserProfiles.Where(a => a.UserName.Equals(objUser.UserName) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.UserId.ToString();
+                        Session["UserName"] = obj.UserName.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                }
+            }
+            return View(objUser);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+
     }
 }
