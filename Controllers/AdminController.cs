2a6aa61fc4d23b89dc32ac74be459d19ef05cf27
Pablo <pablo.munoz@davinci.edu.ar>
2022-12-11 22:30:08 -0300

    creacion de vista de admin

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FINAL_MVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using FINAL_MVC.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace FINAL_MVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly Context _context;
+
+        public AdminController(Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
